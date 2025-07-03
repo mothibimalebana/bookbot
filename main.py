982952da5fe489d53499dcd491b0704c8716b2ea
mothibimalebana <mothibi.mm12@gmail.com>
2025-07-03 22:39:25 +0200

    feat: prints the number of words in book

---
+++ main.py
@@ -0,0 +1,16 @@
+def get_book_test(book_path):
+    with open(book_path) as book:    # do something with book here
+        file_contents = book.read()
+        # print(file_contents)
+    return file_contents
+
+def get_word_count(book):
+    words_array = book.split(" ")
+    word_count = len(words_array)
+    print(f"{word_count} words found in the document")
+
+def main():
+    frankenstein = get_book_test("./books/frankenstein.txt")
+    get_word_count(frankenstein)
+
+main()
